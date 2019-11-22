46b340fbc9567116356693994352d4b15bc898fb
C Barton <43180845+CB987@users.noreply.github.com>
2019-11-21 19:35:14 -0500

    uploading libstats-error-email

---
+++ libstats-error-email.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+
+import os
+
+def main():
+    # want to send an email with the file size of the curl log after it's done running
+    size = os.path.getsize(/ezpaarse/work/curl.log)
+    print size
+    # if ()
+    # send_email(cbarto8@emory.edu, "curl log is " + size + "bytes")
+    # and maybe stop the process if it's not the right size?
+
+if __name_ =="__main__":
+    sys.exit(main())
